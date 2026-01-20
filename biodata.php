2e389896959636a3c551638909f6396dc94bcc29
AmirFawwaz06 <18DIT24F1021@student.ptss.edu.my>
2026-01-20 10:06:08 +0800

    first commit nothing changes

---
+++ biodata.php
@@ -0,0 +1,5 @@
+<?php
+$nama = "Amir Muhammad Fawwaz";
+$nomatrik = "18DIT24F1021";
+$kelas = "DIT4B";
+?>
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php include "biodata.php"; ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Biodata Diri</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="container">
+    <h1>Biodata Diri</h1>
+
+    <img src="myself.jpeg" width="200">
+
+    <p><b>Nama:</b> <?= $nama ?></p>
+    <p><b>No Matrik:</b> <?= $nomatrik ?></p>
+    <p><b>Kelas:</b> <?= $kelas ?></p>
+</div>
+
+</body>
+</html>
